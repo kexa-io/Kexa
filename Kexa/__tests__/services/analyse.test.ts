be206e9088bf133c62c7ab7818c1b1746d16d09d
estebanmathia <74156507+estebanmathia@users.noreply.github.com>
2023-08-04 16:47:51 +0200

    start test unit

---
+++ Kexa/__tests__/services/analyse.test.ts
@@ -0,0 +1,216 @@
+import { ConditionEnum } from "../../enum/condition.enum";
+import { checkEndsWith, checkEqual, checkEqualDate, checkEqualThanDate, checkGreaterThan, checkGreaterThanDate, checkInclude, checkIncludeNS, checkInterval, checkIntervalDate, checkLessThan, checkLessThanDate, checkRegex, checkStartsWith, gatheringRules, generateDate } from "../../services/analyse.service";
+
+const { expect } = require('chai');
+
+describe('analyse service', () => {
+    describe('Gathering rules', () => {
+        it('should return one rule', async () => {
+            const result = await gatheringRules("./Kexa/__tests__/rules/test1");
+            expect(result.length).to.equal(1);
+        });
+
+        it('should return a multiple rules', async () => {
+            const result = await gatheringRules("./Kexa/__tests__/rules/test2");
+            expect(result.length).to.be.above(1);
+        });
+
+        it('should return 0 rules', async () => {
+            const result = await gatheringRules("./Kexa/__tests__/rules/test3");
+            expect(result.length).to.equal(0);
+        });
+    });
+
+    describe("resultScan function", () => {
+        
+        describe("Equal Date", () => {
+            it("should return true", () => {
+                const result = checkEqualDate({property: "date", condition: ConditionEnum.DATE_EQUAL, value: "01-01-2021", date: "DD-MM-YYYY"}, "01-01-2021");
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkEqualDate({property: "date", condition: ConditionEnum.DATE_EQUAL, value: "02-01-2021", date: "DD-MM-YYYY"}, "01-01-2021");
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Interval Date", () => {
+            it("should return true", () => {
+                const result = checkIntervalDate({property: "date", condition: ConditionEnum.DATE_INTERVAL, value: "01-01-2021 02-01-2021", date: "DD-MM-YYYY"}, "01-01-2021");
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkIntervalDate({property: "date", condition: ConditionEnum.DATE_INTERVAL, value: "01-01-2021 02-01-2021", date: "DD-MM-YYYY"}, "03-01-2021");
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Interval", () => {
+            it("should return true", () => {
+                const result = checkInterval({property: "date", condition: ConditionEnum.INTERVAL, value: "1 3"}, 1);
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkInterval({property: "date", condition: ConditionEnum.INTERVAL, value: "1 3"}, 4);
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Greater than date", () => {
+            it("should return true", () => {
+                const result = checkGreaterThanDate({property: "date", condition: ConditionEnum.DATE_SUP, value: "1 0 0 1", date: "DD-MM-YYYY"}, generateDate("0 0 0 2 0 0").format("DD-MM-YYYY"));
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkGreaterThanDate({property: "date", condition: ConditionEnum.DATE_SUP, value: "1 0 0 1", date: "DD-MM-YYYY"}, generateDate("0 0 0 1 0 0").format("DD-MM-YYYY"));
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Less than date", () => {
+            it("should return true", () => {
+                const result = checkLessThanDate({property: "date", condition: ConditionEnum.DATE_INF, value: "0 0 0 1", date: "DD-MM-YYYY"}, generateDate("0 0 0 1 0 0", false).format("DD-MM-YYYY"));
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkLessThanDate({property: "date", condition: ConditionEnum.DATE_INF, value: "0 0 0 1", date: "DD-MM-YYYY"}, generateDate("0 0 0 0 1 0").format("DD-MM-YYYY"));
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Equal than date", () => {
+            it("should return true", () => {
+                //NOT WORKING
+                const result = checkEqualThanDate({property: "date", condition: ConditionEnum.DATE_INF_OR_EQUAL, value: "0 0 0 1", date: "DD-MM-YYYY"}, generateDate("0 0 0 1 0 0", false).format("DD-MM-YYYY"), false);
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkEqualThanDate({property: "date", condition: ConditionEnum.DATE_INF_OR_EQUAL, value: "0 0 0 1", date: "DD-MM-YYYY"}, generateDate("0 0 0 0 0 0").format("DD-MM-YYYY"), false);
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Equal", () => {
+            it("should return true", () => {
+                const result = checkEqual({property: "date", condition: ConditionEnum.EQUAL, value: 1}, 1);
+                expect(result).to.equal(true);
+            })
+
+            it("should return false", () => {
+                const result = checkEqual({property: "date", condition: ConditionEnum.EQUAL, value: 1}, 2);
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Sup", () => {
+            it("should return true", () => {
+                const result = checkGreaterThan({property: "date", condition: ConditionEnum.SUP, value: 1}, 2);
+                expect(result).to.equal(true);
+            })
+
+            it("should return false", () => {
+                const result = checkGreaterThan({property: "date", condition: ConditionEnum.SUP, value: 1}, 1);
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Inf", () => {
+            it("should return true", () => {
+                const result = checkLessThan({property: "date", condition: ConditionEnum.INF, value: 1}, 0);
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkLessThan({property: "date", condition: ConditionEnum.INF, value: 1}, 1);
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Include", () => {
+            it("should return true", () => {
+                const result = checkInclude({property: "date", condition: ConditionEnum.INCLUDE, value: "2"}, "1 2 3");
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkInclude({property: "date", condition: ConditionEnum.INCLUDE, value: "4"}, "1 2 3");
+                expect(result).to.equal(false);
+            });
+            
+            it("should return false", () => {
+                const result = checkInclude({property: "date", condition: ConditionEnum.STARTS_WITH, value: "A"}, "abc");
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Starts with", () => {
+            it("should return true", () => {
+                const result = checkStartsWith({property: "date", condition: ConditionEnum.STARTS_WITH, value: "1"}, "1 2 3");
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkStartsWith({property: "date", condition: ConditionEnum.STARTS_WITH, value: "4"}, "1 2 3");
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Ends with", () => {
+            it("should return true", () => {
+                const result = checkEndsWith({property: "date", condition: ConditionEnum.ENDS_WITH, value: "3"}, "1 2 3");
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkEndsWith({property: "date", condition: ConditionEnum.ENDS_WITH, value: "4"}, "1 2 3");
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Include not sensitive", () => {
+            it("should return true", () => {
+                const result = checkIncludeNS({property: "date", condition: ConditionEnum.INCLUDE_NOT_SENSITIVE, value: "2"}, "1 2 3");
+                expect(result).to.equal(true);
+            });
+
+            it("should return true", () => {
+                const result = checkIncludeNS({property: "date", condition: ConditionEnum.INCLUDE_NOT_SENSITIVE, value: "A"}, "abc");
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkIncludeNS({property: "date", condition: ConditionEnum.INCLUDE_NOT_SENSITIVE, value: "4"}, "1 2 3");
+                expect(result).to.equal(false);
+            });
+        });
+
+        describe("Regex", () => {
+            it("should return true", () => {
+                const result = checkRegex({property: "date", condition: ConditionEnum.REGEX, value: "^[a-z]+$"}, "abc");
+                expect(result).to.equal(true);
+            });
+
+            it("should return false", () => {
+                const result = checkRegex({property: "date", condition: ConditionEnum.REGEX, value: "/[0-9]/gm"}, "abc");
+                expect(result).to.equal(false);
+            });
+        });
+
+    });
+});
+
+//ALL
+//NOT_ANY
+//SOME
+//ONE
+//COUNT
+//COUNT_SUP
+//COUNT_SUP_OR_EQUAL
+//COUNT_INF
+//COUNT_INF_OR_EQUAL
