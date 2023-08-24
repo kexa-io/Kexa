d0232bf2db8108e2afee3ddb988df1eac35cf679
Esteban Mathia <74156507+estebanmathia@users.noreply.github.com>
2023-08-24 14:15:21 +0200

    Delete o365Display.service.ts

--- Kexa/services/addOn/display/o365Display.service.ts
+++
@@ -1,5 +0,0 @@
-import { Rules } from "../../../models/settingFile/rules.models";
-
-export function propertyToSend(rule: Rules, objectContent: any, isSms: boolean=false): string{
-        return `Id : `;
-}
