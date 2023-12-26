bb96aa32d80437dd2b95cee864303eb4b16fae2f
aeppling <adrien.eppling@epitech.eu>
2023-12-26 16:11:13 +0100

    Update azureGathering.service.ts

--- Kexa/services/addOn/azureGathering.service.ts
+++ Kexa/services/addOn/azureGathering.service.ts
@@ -83,32 +83,34 @@ import  {RedisManagementClient} from '@azure/arm-rediscache';
 import {ApplicationInsightsManagementClient} from '@azure/arm-appinsights';
 import {WebSiteManagementClient} from '@azure/arm-appservice';
 import {RecoveryServicesClient} from '@azure/arm-recoveryservices';
+import {ContainerServiceClient} from '@azure/arm-containerservice';
+
+import {ExternalIdentitiesConfigurationClient} from '@azure/arm-azureadexternalidentities';
+import {AdvisorManagementClient} from '@azure/arm-advisor';
+import {ApiManagementClient } from '@azure/arm-apimanagement';
+
+// NEW STARTING FROM HERE (NOT TESTED)
+import { AttestationManagementClient } from '@azure/arm-attestation';
+import { AppPlatformManagementClient } from '@azure/arm-appplatform';
+import { AzureVMwareSolutionAPI } from '@azure/arm-avs';
+import { AzureStackManagementClient } from '@azure/arm-azurestack';
+import { AzureStackHCIClient } from '@azure/arm-azurestackhci';
+import { AutomationClient } from '@azure/arm-automation';
+import { AutomanageClient } from '@azure/arm-automanage';
+import { AutoSuggestClient } from '@azure/cognitiveservices-autosuggest';
+
+
 
 import * as ckiNetworkSecurityClass from "../../class/azure/ckiNetworkSecurityGroup.class";
-import { AzureResources } from "../../models/azure/resource.models";
 import { DefaultAzureCredential } from "@azure/identity";
 import { getConfigOrEnvVar, setEnvVar } from "../manageVarEnvironnement.service";
 import { AzureConfig } from "../../models/azure/config.models";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-const { ContainerServiceClient } = require("@azure/arm-containerservice");
 
 import {getContext, getNewLogger} from "../logger.service";
-import { String } from "aws-sdk/clients/acm";
-import { Bool } from "aws-sdk/clients/clouddirectory";
 const logger = getNewLogger("AzureLogger");
 
-let computeClient: ComputeManagementClient;
-let resourcesClient : ResourceManagementClient ;
-let networkClient: NetworkManagementClient;
-let storageClient: StorageManagementClient;
-let appConfigClient: AppConfigurationManagementClient;
-let sqlClient: SqlManagementClient;
-let postgresClient: PostgreSQLManagementClient;
-let redisClient: RedisManagementClient;
-let insightClient: ApplicationInsightsManagementClient;
-let webappClient: WebSiteManagementClient;
-let recoveryClient: RecoveryServicesClient;
 
 import { exec } from 'child_process';
 
@@ -136,43 +138,43 @@ listAzureArmPackages()
     console.error('Error:', error);
   });
 
+const clientConstructors: Record<string, any> = {
+    ResourceManagementClient,
+    StorageManagementClient,
+    ComputeManagementClient,
+    NetworkManagementClient,
+    AppConfigurationManagementClient,
+    SqlManagementClient,
+    PostgreSQLManagementClient,
+    RedisManagementClient,
+    ApplicationInsightsManagementClient,
+    WebSiteManagementClient,
+    RecoveryServicesClient,
+    ExternalIdentitiesConfigurationClient,
+    AdvisorManagementClient,
+    ApiManagementClient
+   /* AttestationManagementClient,
+    AppPlatformManagementClient,
+    AzureVMwareSolutionAPI,
+    AzureStackManagementClient,
+    AzureStackHCIClient,
+    AutomationClient,
+    AutomanageClient,
+    AutoSuggestClient*/
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// LISTING CLOUD RESOURCES
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-export async function collectData(azureConfig:AzureConfig[]): Promise<AzureResources[]|null>{
+export async function collectData(azureConfig:AzureConfig[]): Promise<Object[]|null>{
     let context = getContext();
-    let resources = new Array<AzureResources>();
+    let resources = new Array<Object>();
     for(let config of azureConfig??[]){
-        let azureResource = {
-            "vm": null,
-            "rg": null,
-            "disk": null,
-            "nsg": null,
-            "virtualNetwork": null,
-            "networkInterfaces": null,
-            "aks": null,
-            "storage": null,
-            "blob": null,
-            "firewall": null,
-            "route_table": null,
-            "network_interface": null,
-            "express_route": null,
-            "private_endpoint": null,
-            "all": null,
-            "insight": null,
-            "gateway": null,
-            "service": null,
-            "backup": null,
-            "sql": null,
-            "postgres": null,
-            "redis": null
-        } as AzureResources;
         logger.debug("config: ");
         logger.debug(JSON.stringify(config));
         let prefix = config.prefix??(azureConfig.indexOf(config).toString());
         try{
-            logger.debug("prefix: "+prefix);
+            logger.debug("prefix: " + prefix);
             let subscriptionId = await getConfigOrEnvVar(config, "SUBSCRIPTIONID", prefix);
             let azureClientId = await getConfigOrEnvVar(config, "AZURECLIENTID", prefix);
             if(azureClientId) setEnvVar("AZURE_CLIENT_ID", azureClientId);
@@ -190,71 +192,28 @@ export async function collectData(azureConfig:AzureConfig[]): Promise<AzureResou
 
             if(!subscriptionId) {
                 throw new Error("- Please pass "+ prefix + "SUBSCRIPTIONID in your config file");
-            }else{
-                //getting clients for azure
-                resourcesClient = new ResourceManagementClient(credential, subscriptionId);
-                computeClient   = new ComputeManagementClient(credential, subscriptionId);
-                networkClient   = new NetworkManagementClient(credential, subscriptionId);
-                sqlClient = new SqlManagementClient(credential, subscriptionId);
-                postgresClient = new PostgreSQLManagementClient(credential, subscriptionId);
-                redisClient = new RedisManagementClient(credential, subscriptionId);
-                insightClient = new ApplicationInsightsManagementClient(credential, subscriptionId);
-                webappClient = new WebSiteManagementClient(credential, subscriptionId);
-                recoveryClient = new RecoveryServicesClient(credential, subscriptionId);
-
+            } else {
                 context?.log("- loading client microsoft azure done-");
                 logger.info("- loading client microsoft azure done-");
                 ///////////////// List cloud resources ///////////////////////////////////////////////////////////////////////////////////////////////
-
-                const promises = [
-                    await networkSecurityGroupListing(networkClient),
-                    await virtualMachinesListing(computeClient),
-                    await resourceGroupListing(resourcesClient),
-                    await disksListing(computeClient),
-                    await virtualNetworksListing(networkClient),
-                    await aksListing(credential, subscriptionId),
-                    await ipListing(networkClient),
-                    await listAllStorage(storageClient),
-                    await listAllBlob(storageClient, credential),
-                    await listAzureFirewall(networkClient),
-                    await listRouteTable(networkClient),
-                    await listNetworkInterfaces(networkClient),
-                    await listExpressRoute(networkClient),
-                    await listPrivateEndpoint(appConfigClient),
-                    await listAllResources(resourcesClient),
-                    await listAppInsight(insightClient),
-                    await listAppGateway(networkClient),
-                    await listAppService(webappClient),
-                    await listAzBackup(recoveryClient),
-                    await listSQL(sqlClient),
-                    await listPostgres(postgresClient),
-                    await listRedis(redisClient),
-                    //getSPKeyInformation(credential, subscriptionId)
-                ];
-                
-                const [nsgList, vmList, rgList, diskList, virtualNetworkList,
-                    aksList, ipList, storageList, blobList, firewallList, routeList,
-                interfaceList, expressList, pvEndpointList, allList, insightList, gatewayList,
-                serviceList, backupList, sqlList, postgresList, redisList] = await Promise.all(promises); //, SPList
-                context?.log("- listing cloud resources done -");
-                logger.info("- listing cloud resources done -");
-                azureResource = {
-                    "vm": [...azureResource["vm"]??[], ...vmList],
-                    "rg": [...azureResource["rg"]??[], ...rgList],
-                    "disk": [...azureResource["disk"]??[], ...diskList],
-                    "nsg": [...azureResource["nsg"]??[], ...nsgList],
-                    "virtualNetwork": [...azureResource["virtualNetwork"]??[], ...virtualNetworkList],
-                    "aks": [...azureResource["aks"]??[], ...aksList],
-                    "ip": [...azureResource["ip"]??[], ...ipList],
-                    //"sp": [...azureResource["sp"]??[], ...SPList],
-                } as AzureResources;
+                interface AzureRet {
+                    [key: string]: any;
+                }
+                const azureRet: AzureRet = {};
+                for (const clientService in clientConstructors) {
+                    const constructor = clientConstructors[clientService];
+                    const clientName = constructor.name;
+                    azureRet[clientName] = await callGenericClient(createGenericClient(constructor, credential, subscriptionId));
+                }
+                console.log(azureRet);
+                resources.push(azureRet);
             }
-        }catch(e){
+        } catch(e) {
             logger.error("error in collectAzureData with the subscription ID: " + (await getConfigOrEnvVar(config, "SUBSCRIPTIONID", prefix))??null);
             logger.error(e);
         }
-        resources.push(azureResource);
     }
+    console.log(resources);
     return resources??null;
 }
 
@@ -273,149 +232,6 @@ export async function getSPKeyInformation(credential: DefaultAzureCredential, su
         logger.debug("error in getSPKeyInformation:"+err);
         return null;
     }
-}
-
-//ip list
-export async function ipListing(client:NetworkManagementClient): Promise<Array<any>|null> {
-    logger.info("starting ipListing");
-    try{
-        const resultList = new Array<any>;
-        for await (const item of client.publicIPAddresses.listAll()) {
-            resultList.push(item);
-        }
-        return resultList;
-    }catch(e){
-        logger.debug("error in ipListing:"+e);
-        return null;
-    }
-}
-
-//aks list
-export async function aksListing(credential: DefaultAzureCredential, subscriptionId: string): Promise<any> {
-    logger.info("starting aksListing");
-    try{
-        const client = new ContainerServiceClient(credential, subscriptionId);
-        const resArray = new Array();
-        for await (let item of client.managedClusters.list()) {
-            resArray.push(item);
-        }
-        return resArray;
-    }catch(e){
-        logger.debug("error in aksListing:"+e);
-        return null;
-    }
-}
-
-//network security group list
-export async function networkSecurityGroupListing(client:NetworkManagementClient): Promise<Array<NetworkSecurityGroup>|null> {
-    logger.info("starting networkSecurityGroupListing");
-    try {
-        const resultList = new Array<NetworkSecurityGroup>;
-        //const test = await client.networkSecurityGroups.listAll();
-        for await (const item of client.networkSecurityGroups.listAll()) {
-            resultList.push(item);
-        }
-        logger.info("ended networkSecurityGroupListing");
-        console.log(resultList);
-        return resultList;        
-    } catch (err) {
-        logger.debug("error in networkSecurityGroupListing:"+err);
-        return null;
-    }
-}
-
-//virtual network list
-export async function virtualNetworksListing(client:NetworkManagementClient): Promise<Array<VirtualNetwork>|null> {
-    logger.info("starting virtualNetworksListing");
-    try {
-        const resultList = new Array<VirtualNetwork>;
-        for await (const item of client.virtualNetworks.listAll()) {
-            resultList.push(item);
-        }
-
-        return resultList;
-    } catch (err) {
-        logger.debug("error in virtualNetworksListing:"+err);
-        return null;
-    }
-}
-
-//network list
-export async function networkInterfacesListing(client:NetworkManagementClient): Promise<Array<NetworkInterface>|null> {
-    logger.info("starting networkInterfacesListing");
-    try {
-        const resultList = new Array<NetworkInterface>;
-        for await (const item of client.networkInterfaces.listAll()) {
-            resultList.push(item);
-        }
-        return resultList;
-    } catch (err) {
-        logger.debug("error in networkInterfacesListing:"+err);
-        return null;
-    }
-}
-
-//disks.list
-export async function disksListing(client:ComputeManagementClient): Promise<Array<Disk>|null> {
-    logger.info("starting disksListing");
-    try {
-        const resultList = new Array<Disk>;
-        for await (const item of client.disks.list()) {
-            resultList.push(item);
-        }
-        console.log(resultList);
-        return resultList;
-    } catch (err) {
-        logger.debug("error in disksListing:"+err);
-        return null;
-    }    
-}
-
-//virtualMachines.listAll
-export async function virtualMachinesListing(client:ComputeManagementClient): Promise<Array<VirtualMachine>|null> {
-    logger.info("starting virtualMachinesListing");
-    try {
-        const resultList = new Array<VirtualMachine>;
-        for await (let item of client.virtualMachines.listAll()){
-            resultList.push(item);
-        }
-        return resultList;
-    }catch (err) {
-        logger.debug("error in virtualMachinesListing:"+err);
-        return null;
-    } 
-}
-
-export async function resourceGroupListing(client:ResourceManagementClient): Promise<Array<ResourceGroup>|null> {
-    logger.info("starting resourceGroupListing");
-    try {
-        const resultList = new Array<ResourceGroup>;
-        for await (let item of client.resourceGroups.list()){
-            resultList.push(item);
-        }
-        return resultList;
-    }catch (err) {
-        logger.debug("error in resourceGroupListing:"+err);
-        return null;
-    }     
-}
-
-export async function listAllStorage(client:StorageManagementClient): Promise<Array<StorageAccount>|null> {
-    logger.info("starting listAllStorage");
-    try {
-        const resultList = new Array<ResourceGroup>;
-        const test = await client.storageAccounts.list();
-        console.log("storage :", test);
-        for await (let item of client.storageAccounts.list()){
-            resultList.push(item);
-        }
-        return resultList;
-    }catch (err) {
-        logger.debug("error in resourceGroupListing:"+err);
-        return null;
-    }
-
-    return null;
 }
 
 export async function listAllBlob(client:StorageManagementClient, credentials: any): Promise<Array<StorageAccount>|null> {
@@ -444,97 +260,6 @@ export async function listAllBlob(client:StorageManagementClient, credentials: a
         return null;
     }
 
-    return null;
-}
-
-export async function listAzureFirewall(client:NetworkManagementClient): Promise<Array<StorageAccount>|null> {
-    logger.info("starting list Azure Firewall");
-    try {
-        const resultList = new Array<ResourceGroup>;
-        const test = await client.azureFirewalls.listAll();
-        console.log("firewalls :", test);
-      /*  for await (let item of client.azureFirewalls.list()){
-            resultList.push(item);
-        }*/
-        return resultList;
-    }catch (err) {
-        logger.debug("error in resourceGroupListing:"+err);
-        return null;
-    }
-
-    return null;
-}
-
-export async function listRouteTable(client:NetworkManagementClient): Promise<Array<StorageAccount>|null> {
-    logger.info("starting list route table");
-    try {
-        const resultList = new Array<ResourceGroup>;
-        const test = await client.routeTables.listAll();
-        console.log("route table :", test);
-        /*  for await (let item of client.azureFirewalls.list()){
-              resultList.push(item);
-          }*/
-        return resultList;
-    }catch (err) {
-        logger.debug("error in resourceGroupListing:"+err);
-        return null;
-    }
-
-    return null;
-}
-
-export async function listNetworkInterfaces(client:NetworkManagementClient): Promise<Array<StorageAccount>|null> {
-    logger.info("starting list network interface");
-    try {
-        const resultList = new Array<ResourceGroup>;
-        const test = await client.networkInterfaces.listAll();
-        console.log("network interface :", test);
-        /*  for await (let item of client.azureFirewalls.list()){
-              resultList.push(item);
-          }*/
-        return resultList;
-    }catch (err) {
-        logger.debug("error in network interface:"+err);
-        return null;
-    }
-    return null;
-}
-
-export async function listExpressRoute(client:NetworkManagementClient): Promise<Array<StorageAccount>|null> {
-    logger.info("starting list express route");
-    try {
-        const resultList = new Array<ResourceGroup>;
-        const test = await client.expressRouteCircuits.listAll();
-        console.log("express route :", test);
-        /*  for await (let item of client.azureFirewalls.list()){
-              resultList.push(item);
-          }*/
-        return resultList;
-    }catch (err) {
-        logger.debug("error in express route:"+err);
-        return null;
-    }
-    return null;
-}
-
-export async function listPrivateEndpoint(client:AppConfigurationManagementClient): Promise<Array<StorageAccount>|null> {
-    logger.info("starting list private endpoint");
-    console.log("list store :");
-
-    try {
-        const resultList = new Array<ResourceGroup>;
-        console.log("list store :");
-        const test = client.configurationStores.list();
-        console.log("config store :");
-        console.log(test);
-        /*  for await (let item of client.azureFirewalls.list()){
-              resultList.push(item);
-          }*/
-        return resultList;
-    }catch (err) {
-        logger.debug("error in private endpoint:"+err);
-        return null;
-    }
     return null;
 }
 
@@ -591,21 +316,26 @@ async function listPostgres(resourcesClient: PostgreSQLManagementClient): Promis
 
 async function listRedis(resourcesClient: RedisManagementClient): Promise<Array<any>|null> {
     await callGenericClient(resourcesClient);
+
     return null;
+}
+
+function createGenericClient<T>(Client: new (credential: any, subscriptionId: any) => T, credential: any, subscriptionId: any): T {
+    return new Client(credential, subscriptionId);
 }
 
 async function callGenericClient(client: any) {
     logger.info("starting " + client.constructor.name + " Listing");
     let results = await listAllResources(client);
-    logger.trace(results);
+    logger.info(results);
     return results;
 }
 
 async function listAllResources(client: any) {
     logger.trace("Automatic gathering...");
     const properties = Object.getOwnPropertyNames(client);
-    logger.silly("Properties of client:");
-    logger.silly(properties);
+    logger.trace("Properties of client:");
+    logger.trace(properties);
 
     const resultList: Record<string, any> = {};
 
@@ -643,7 +373,6 @@ async function listAllResources(client: any) {
             );
         }
     });
-
     await Promise.all(promises);
     return resultList;
 }
