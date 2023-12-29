ce157a34695fbb403686b85fb57767b5a8aef4b9
aeppling <adrien.eppling@epitech.eu>
2023-12-29 17:08:43 +0100

    backup

--- Kexa/models/azure/resource.models.ts
+++ Kexa/models/azure/resource.models.ts
@@ -1,42 +1,21 @@
-import { Disk, VirtualMachine } from "@azure/arm-compute";
-import { NetworkSecurityGroup, VirtualNetwork } from "@azure/arm-network";
-import { ResourceGroup } from "@azure/arm-resources";
+import { NetworkSecurityGroup } from "@azure/arm-network";
 
 export interface AzureResources {
-    vm: Array<NetworkSecurityGroup>|null;
-    rg: Array<VirtualMachine>|null;
-    disk: Array<Disk>|null;
-    nsg: Array<ResourceGroup>|null;
-    virtualNetwork: Array<VirtualNetwork>|null;
-    networkInterfaces: Array<NetworkSecurityGroup>|null;
-    namespaces: Array<any>|null;
-    pods: Array<any>|null;
-    aks: Array<any>|null;
-    ip: Array<any>|null;
-    mlWorkspaces: Array<any>|null;
-    mlJobs: Array<any>|null;
-    mlComputes: Array<any>|null;
-    mlSchedules: Array<any>|null;
-    storage: Array<any>|null;
-
-    blob: Array<any>|null;
-
-    firewall: Array<any>|null;
-    route_table: Array<any>|null;
-
-    network_interface: Array<any>|null;
-
-    express_route: Array<any>|null;
-
-    private_endpoint: Array<any>|null;
-    all: Array<any>|null;
-    insight: Array<any>|null;
-    gateway: Array<any>|null;
-    service: Array<any>|null;
-
-    backup: Array<any>|null;
-
-    sql: Array<any>|null;
-    postgres: Array<any>|null;
-    redis: Array<any>|null;
+    'KexaAzure.vm': Array<NetworkSecurityGroup>|null;
+    'KexaAzure.mlWorkspaces': Array<any>|null;
+    'KexaAzure.mlJobs': Array<any>|null;
+    'KexaAzure.mlComputes': Array<any>|null;
+    'KexaAzure.mlSchedules': Array<any>|null;
+    'KexaAzure.storage': Array<any>|null;
+    'KexaAzure.blob': Array<any>|null;
 }
+
+export const stringKeys: Array<String> = [
+    'KexaAzure.vm',
+    'KexaAzure.mlWorkspaces',
+    'KexaAzure.mlJobs',
+    'KexaAzure.mlComputes',
+    'KexaAzure.mlSchedules',
+    'KexaAzure.storage',
+    'KexaAzure.blob',
+];
