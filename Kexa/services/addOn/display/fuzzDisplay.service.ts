bf4a69b47bcfb02933175b2b11ad44fe78df5bdc
aesoft <43991222+aeppling@users.noreply.github.com>
2024-09-06 11:30:54 +0200

    fix: add display service for fuzzing addon

---
+++ Kexa/services/addOn/display/fuzzDisplay.service.ts
@@ -0,0 +1,10 @@
+import { Rules } from "../../../models/settingFile/rules.models";
+
+export function propertyToSend(rule: Rules, objectContent: any, isSms: boolean=false): string {
+    switch (rule?.objectName) {
+        case "request":
+            return `Url : ` + objectContent?.url + ` with status : ` + objectContent?.code
+        default:
+            return 'HTTP Scan : Id : ' + objectContent.id;
+    }
+}
