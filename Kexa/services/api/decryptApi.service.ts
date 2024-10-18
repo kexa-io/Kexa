b57d2b744827e45995ffedb62547b68d14add815
aesoft <43991222+aeppling@users.noreply.github.com>
2024-10-18 13:07:11 +0200

    fix: condition for no encrypt config

--- Kexa/services/api/decryptApi.service.ts
+++ Kexa/services/api/decryptApi.service.ts
@@ -4,21 +4,28 @@ import {config} from './encryption.config'
 
 const { secret_key, secret_iv, ecnryption_method } = config
 
-if (!secret_key || !secret_iv || !ecnryption_method) {
-  throw new Error('secretKey, secretIV, and ecnryptionMethod are required')
+// if ((process.env.INTERFACE_CONFIGURATION_ENABLED == 'true') && (!secret_key || !secret_iv || !ecnryption_method)) {
+//   throw new Error('secretKey, secretIV, and ecnryptionMethod are required')
+// }
+
+let key: string = '';
+let encryptionIV: string = '';
+
+if (process.env.INTERFACE_CONFIGURATION_ENABLED == 'true') { 
+  if (!secret_key || !secret_iv || !ecnryption_method) {
+    throw new Error('secretKey, secretIV, and ecnryptionMethod are required')
+  }
+  key = crypto
+    .createHash('sha512')
+    .update(secret_key)
+    .digest('hex')
+    .substring(0, 32)
+  encryptionIV = crypto
+    .createHash('sha512')
+    .update(secret_iv)
+    .digest('hex')
+    .substring(0, 16)
 }
-
-const key = crypto
-  .createHash('sha512')
-  .update(secret_key)
-  .digest('hex')
-  .substring(0, 32)
-const encryptionIV = crypto
-  .createHash('sha512')
-  .update(secret_iv)
-  .digest('hex')
-  .substring(0, 16)
-
 
 export function decryptData(encryptedData: any) {
   const buff = Buffer.from(encryptedData, 'base64')
