29c95654d7e779f2ad99a0484a2d368df55acedb
aesoft <43991222+aeppling@users.noreply.github.com>
2025-05-27 13:32:37 +0200

    fix: no "should upgrade" message

--- Kexa/helpers/latestVersion.ts
+++ Kexa/helpers/latestVersion.ts
@@ -34,27 +34,5 @@ export async function displayVersionAndLatest(logger: any): Promise<void>{
             next = false;
         }
     });
-    displayUpgrade(logger, betterUpgrade, currentVersion, latestVersionInGithub);
     return Promise.resolve();
-}
-
-async function displayUpgrade(logger: any, betterUpgrade: number, currentVersion: string, latestVersionInGithub: string|null): Promise<void>{
-    const command = [
-        ["bash ./initKexa.sh -d", "bash"],
-        ["powershell ./initKexa.ps1 -d", "powershell"]
-    ];
-    logger.info(`Current version: ${currentVersion} ${betterUpgrade != -1 ? "" : "(latest)"}`);
-    if(betterUpgrade != -1){
-        logger.info(`Latest version in Github: ${latestVersionInGithub}\nYou should upgrade to the latest version to get the best experience.`);
-        if(betterUpgrade == 0) logger.info(`It's a major upgrade, be aware that some breaking changes may have been made.`);
-        if(betterUpgrade != 0 && ((await getEnvVar("AUTOUPDATE"))??false)){
-            const { spawn } = require('child_process')
-            spawn(command[process.platform == "win32" ? 1:0][0], [], { shell: true, stdio: 'inherit' })
-            spawn("npm run build", [], { shell: true, stdio: 'inherit' });
-            logger.info(`your Kexa is now up to date, restart it to enjoy changes.`);
-        }else{
-            logger.info(`Please consider this command to upgrade : "${command[process.platform == "win32" ? 1:0][0]}" (${command[process.platform == "win32" ? 1:0][1]})`);
-            logger.info(`If you want to upgrade manually, please check the release page on Github.`);
-        }
-    }
 }
