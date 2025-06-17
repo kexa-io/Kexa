34bfc686ace576e704d1ba57e03562e1eefa56ca
aesoft <43991222+aeppling@users.noreply.github.com>
2025-06-17 14:06:08 +0200

    fix(config): get only save from api for sync

--- Kexa/helpers/loaderConfig.ts
+++ Kexa/helpers/loaderConfig.ts
@@ -2,10 +2,10 @@ import {getConfigFromApi} from "../services/api/loaderApi.service";
 
 import * as fs from 'fs';
 
-export async function getConfig(): Promise<any> {
+export async function getConfig(saveOnly: boolean = false): Promise<any> {
 
     if (process.env.INTERFACE_CONFIGURATION_ENABLED == 'true') {
-         return await getConfigFromApi();
+         return await getConfigFromApi(true);
     }
     if (process.env.SPECIALCONFIG) {
         return JSON.parse(fs.readFileSync(process.env.SPECIALCONFIG));
