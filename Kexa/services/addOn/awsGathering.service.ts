0454bd761555677a6376365610d71da90bb35b14
aesoft <43991222+aeppling@users.noreply.github.com>
2025-10-28 14:18:26 +0100

    feat(aws): cis foundations rules + kms dependencies

--- Kexa/services/addOn/awsGathering.service.ts
+++ Kexa/services/addOn/awsGathering.service.ts
@@ -5176,6 +5176,7 @@ export async function collectData(awsConfig: AwsConfig[]): Promise<Object[]|null
                 logger.info("- Listing AWS resources done -");
 				
 				const concatedResults = concatAllObjects(collectedResults);
+				
                 resources.push(concatedResults);
             }
         } catch (e) {
@@ -5281,6 +5282,20 @@ let awsGatherDependencies = [
 			},
 		],
 		matchingError: "SecretsManagerServiceException",
+		functions: {}
+	},
+	{
+		client: "clientkms",
+		objects: [
+			{ 
+				name: "Keys",
+				subGatherName: "KeyMetadata",
+				toSend: "KeyId",
+				toFill: "KeyId",
+				results: []
+			}
+		],
+		matchingError: "KMSServiceException",
 		functions: {}
 	}
 ]
